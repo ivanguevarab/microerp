9c1f5dc2b74d9e6d616890be452a2f523483f35f
ivanguevarab <ivan.guevara.b@gmail.com>
2026-08-16 18:20:37 -0500

    Delete query_test.js

--- query_test.js
+++
@@ -1,3 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-// Necesito la URL y KEY de Supabase, pero no la tengo en bash fácilmente sin leer config.js.
-// Mejor busco la tabla en otros archivos HTML.
